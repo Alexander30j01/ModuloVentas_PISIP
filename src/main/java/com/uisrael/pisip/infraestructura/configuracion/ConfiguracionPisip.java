c74548a829c5def29c3169833aeb79a98357ba43
Joel7736 <e0402113062@uisrael.edu.ec>
2026-07-14 19:14:00 -0500

    Update ConfiguracionPisip.java

--- src/main/java/com/uisrael/pisip/infraestructura/configuracion/ConfiguracionPisip.java
+++ src/main/java/com/uisrael/pisip/infraestructura/configuracion/ConfiguracionPisip.java
@@ -2,9 +2,21 @@ package com.uisrael.pisip.infraestructura.configuracion;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.uisrael.pisip.aplicacion.casouso.entrada.IDocumentacionUseCase;
+import com.uisrael.pisip.aplicacion.casouso.entrada.ISolicitudCreditoUseCase;
+import com.uisrael.pisip.aplicacion.casouso.entrada.IUsuariosUseCase;
+import com.uisrael.pisip.aplicacion.casouso.entrada.IVisitaUseCase;
+
 import com.uisrael.pisip.aplicacion.casouso.impl.DocumentacionUseCaseImpl;
+import com.uisrael.pisip.aplicacion.casouso.impl.SolicitudCreditoUseCaseImpl;
+import com.uisrael.pisip.aplicacion.casouso.impl.UsuariosUseCaseImpl;
+import com.uisrael.pisip.aplicacion.casouso.impl.VisitaUseCaseImpl;
+
 import com.uisrael.pisip.dominio.repositorio.IDocumentacionRepositorio;
+import com.uisrael.pisip.dominio.repositorio.ISolicitudCreditoRepositorio;
+import com.uisrael.pisip.dominio.repositorio.IUsuariosRepositorio;
+import com.uisrael.pisip.dominio.repositorio.IVisitaRepositorio;
 
 @Configuration
 public class ConfiguracionPisip {
