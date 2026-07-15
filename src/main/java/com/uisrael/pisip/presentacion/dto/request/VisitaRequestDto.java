70adc88c756171eb28c26e8d771aad295f628281
Joel7736 <e0402113062@uisrael.edu.ec>
2026-07-14 19:12:04 -0500

    Update VisitaRequestDto.java

--- src/main/java/com/uisrael/pisip/presentacion/dto/request/VisitaRequestDto.java
+++ src/main/java/com/uisrael/pisip/presentacion/dto/request/VisitaRequestDto.java
@@ -6,19 +6,19 @@ import lombok.Data;
 
 @Data
 public class VisitaRequestDto {
-
+     @NotBlank
     private int idVisita;
-
+    @NotBlank
     private Instant ingreso;
-
+    @NotBlank
     private Instant salida;
-
+    @NotBlank
     private double latitud;
-
+    @NotBlank
     private double longitud;
-
+    @NotBlank
     private String observacion;
-
+    @NotBlank
     private boolean estado;
 
 }
