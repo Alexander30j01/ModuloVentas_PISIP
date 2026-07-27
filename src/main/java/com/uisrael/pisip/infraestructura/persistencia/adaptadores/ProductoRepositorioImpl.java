b83c8148e4278074af7b190481ed82d0243fc2d3
Alexander Patiño <alexandermoreta173@gmail.com>
2026-07-26 20:39:28 -0500

    Errores resueltos

--- src/main/java/com/uisrael/pisip/infraestructura/persistencia/adaptadores/ProductoRepositorioImpl.java
+++ src/main/java/com/uisrael/pisip/infraestructura/persistencia/adaptadores/ProductoRepositorioImpl.java
@@ -2,14 +2,12 @@ package com.uisrael.pisip.infraestructura.persistencia.adaptadores;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.stereotype.Component;
 import com.uisrael.pisip.dominio.entidades.Producto;
 import com.uisrael.pisip.dominio.repositorio.IProductoRepositorio;
 import com.uisrael.pisip.infraestructura.persistencia.jpa.ProductoEntity;
 import com.uisrael.pisip.infraestructura.persistencia.mapeadores.IProductoJpaMapper;
 import com.uisrael.pisip.infraestructura.repositorios.IProductoJpaRepositorio;
-@Component
+
 public class ProductoRepositorioImpl implements IProductoRepositorio {
 
 	private final IProductoJpaRepositorio jpaRepositorio;
@@ -42,11 +40,11 @@ public class ProductoRepositorioImpl implements IProductoRepositorio {
 	public void eliminar(int idProducto) {
 		jpaRepositorio.deleteById(idProducto);
 	}
-	
+
 	@Override
 	public void actualizar(Producto producto) {
-	    ProductoEntity entidad = entitytMapper.toEntity(producto);
-	    jpaRepositorio.save(entidad); // JPA sabe que es un update si el ID ya existe
+		ProductoEntity entidad = entitytMapper.toEntity(producto);
+		jpaRepositorio.save(entidad); // JPA sabe que es un update si el ID ya existe
 	}
 
 }
